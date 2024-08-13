e8736021b4c71b771546356cc3b20454bfea4099
lawrence_com <aceliuchanghong@gmail.com>
2024-08-13 09:12:00 +0800

    docs: 修改文字

--- pdf2word_ui.py
+++ pdf2word_ui.py
@@ -39,20 +39,22 @@ def create_app():
             with gr.Column(scale=5):
                 pdf_preview = gr.Image(label='PDF预览', height=600)
         with gr.Row():
-            with gr.Column(scale=6):
+            with gr.Column(scale=5):
                 with gr.Accordion("基本参数设置", open=False):
                     pdf_ocr_mode = gr.Dropdown(label='文档识别模式', choices=['auto', 'ocr', 'txt'], value='auto',
-                                               interactive=True, info='效果不好建议尝试ocr')
-                    threshold_slider = gr.Slider(label="置信度", minimum=0.1, maximum=0.9, value=0.5, interactive=True,
-                                                 step=0.1, info="设置置信度")
-                    save_pic_or_table_data = gr.Dropdown(label='pdf转化为word时,其中表格是否保存为图片到word',
-                                                         choices=['是', '否'], value='是', interactive=True,
-                                                         info='否-则保存为表格')
-                    translate_it = gr.Dropdown(label='是否翻译文档', choices=['否', '是'], value='否', interactive=True,
+                                               interactive=True, info='默认auto,若效果不好建议尝试ocr')
+                    threshold_slider = gr.Slider(label="置信度(默认:0.5)", minimum=0.1, maximum=0.9, value=0.5,
+                                                 interactive=True, step=0.1,
+                                                 info="识别置信度,值越大结果越准确,但是可能忽略很多不准确的结果")
+                    save_pic_or_table_data = gr.Dropdown(label='表格保存为图片', choices=['是', '否'], value='是',
+                                                         interactive=True,
+                                                         info='pdf转化为word时,其中表格是否保存为图片,默认是,否-则保存为表格')
+                    translate_it = gr.Dropdown(label='翻译文档', choices=['否', '是'], value='否', interactive=True,
                                                info='翻译文档成指定语言')
-                    language = gr.Dropdown(label='选择要翻译成什么语言的文档', choices=['English', '中文'],
-                                           value='中文', interactive=True, info='翻译文档语言选择', visible=False)
-            with gr.Column(scale=4):
+                    language = gr.Dropdown(label='选择翻译语言', choices=['English', '中文'],
+                                           value='中文', interactive=True, info='选择要翻译为的语言',
+                                           visible=False)
+            with gr.Column(scale=5):
                 generate_button = gr.Button("开始转化", variant='primary',
                                             icon='./files/archery_bow_arch_sport_icon_228555.ico')
         with gr.Row():
@@ -61,27 +63,26 @@ def create_app():
             with gr.Column(scale=1):
                 download = gr.Button("下载", variant='secondary', visible=False)
             file_new_preview = gr.Markdown(label='转化预览', value='### 初始默认', visible=False)
+        with gr.Row():
+            pdf2md_hide = gr.Dropdown(label='pdf转化的默认md文件,此处仅存储', visible=False)
 
         def update_language(mode):
             language_visible = True if mode == '是' else False
             return gr.update(visible=language_visible)
-
-        def download_new_file(file_path):
-            return file_path
 
         # 第一部分
         file_ori.change(lambda x: pdf_to_image(x), inputs=[file_ori], outputs=[pdf_preview])
         # 第二部分
         translate_it.change(fn=update_language, inputs=[translate_it], outputs=[language])
         # 第三部分
-        download.click(download_new_file, inputs=[file_new], outputs=[file_new])
+        download.click(lambda x: x, inputs=[file_new], outputs=[file_new])
     return demo
 
 
 if __name__ == '__main__':
     """
     python pdf2word_ui.py
-    nohup python pdf2word_ui.py>0.log &
+    nohup python pdf2word_ui.py>ss_pdf2word_ui.log &
     """
     upload_default_path = './z_pdf2word_default_dir'
     if not os.path.exists(upload_default_path):
